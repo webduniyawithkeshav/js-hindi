01b6c308814e598818a9a274435f6784943ac0a0
webduniyawithkeshav <131442164+webduniyawithkeshav@users.noreply.github.com>
2023-12-16 20:15:55 +0000

    container config

---
+++ basics/variables.js
@@ -0,0 +1,8 @@
+const accountId=14435
+let accountEmail="abvd@gmail.com"
+var accountPassword="12335"
+accountCity="jaipur"
+//accountaccountIdId=2
+accountPassword="12445"
+accountCity="Bengaluru"
+console.log(accountPassword);
